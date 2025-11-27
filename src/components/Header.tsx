67a6055d7b936176cc89bfddac9a49f8e90eb8ad
jay.thummar <jay.thummar@rapidops.com>
2025-11-27 15:02:26 +0530

    updated code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { ShoppingCart, User, Download } from "lucide-react";
 import { Link } from "react-router-dom";
-import logoSvg from "/logo.svg";
 
 const Header = () => {
   return (
@@ -10,7 +9,7 @@ const Header = () => {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <Link to="/" className="flex-shrink-0">
-            <img src={logoSvg} alt="Flipkart" className="h-8 w-auto" />
+            <img src="/logo.svg?url" alt="Flipkart" className="h-8 w-auto" />
           </Link>
 
           {/* Right Side Icons */}
